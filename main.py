7ca7520560f69a5fb1414e42273ad8448a1e58a2
Rahulk280604 <123786554+Rahulk280604@users.noreply.github.com>
2025-10-20 12:13:15 +0530

    part3

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import eel
 import sys
 
+
+from engine.features import *
+
+
 eel.init("www")
+
+playAssistantSound()
+
+
 
 edge_path = r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
 eel.start('index.html', mode=edge_path, host='localhost', port=8000, block=True)
